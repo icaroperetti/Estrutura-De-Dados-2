c9433b08118f60c14562a0be4a44e5acd5e9589c
Icaro Peretti <icaroperetti50@gmail.com>
2020-10-24 10:55:41 -0300

    Inserindo,buscando e removendo

--- abb.c
+++ abb.c
@@ -85,25 +85,77 @@ void verify(Nodo* temp){
     }
 }
 
+Nodo* searchMin(Nodo* root){
+    if(root != NULL){
+        Nodo* aux = root;
+        while(root->left != NULL){
+            aux = aux->left;
+            return aux;
+        }
+    }
+    return NULL;
+}
+
+Nodo* delete(Nodo* root,int data){
+    if(root != NULL){
+        if(data > root->data){
+            root->right = delete(root->right,data);
+        }else if (data < root->data){
+            root->left = delete(root->left,data);
+        }else{
+            //Se o nó não tiver nenhum filho, da um free
+            if((root->left == NULL) && (root->right == NULL)){
+                free(root);
+                return NULL;
+            }else{
+                //Se tiver filho a direita, guarda em uma variavel auxiliar
+                //e da um free no pai e retorna o aux que ocupa o lugar vazio
+                if(root->left == NULL && root->right != NULL){
+                   Nodo* aux = root->right;
+                   free(root); 
+                   return aux;
+                }else{
+                    //Se tiver filho a esquerda, guarda em uma variavel auxiliar
+                    //e da um free no pai e retorna o aux que ocupa o lugar vazio
+                    if(root->right == NULL && root->left != NULL ){
+                        Nodo* aux = root->left;
+                        free(root);
+                        return aux;
+                    }else{
+                        Nodo* aux = searchMin(root); //Buscando menor valor da arvore
+                        Nodo* keep = aux->data; //Faz uma copia do menor valor da arvore
+                        root = delete(root,keep->data);
+                        root->data = keep;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+    return NULL;
+}
+
 int main(){
     int inserir;
     Nodo* root = inicialize();
-    
-    printf("Digite o numero que desenha inserir\n");
-    scanf("%d",&inserir);
-    root = insertNode(root,inserir);
 
-    printf("Digite o numero que desenha inserir\n");
-    scanf("%d",&inserir);
-    root = insertNode(root,inserir);
+    // printf("Digite o numero que desenha inserir\n");
+    // scanf("%d",&inserir);
+    // root = insertNode(root,inserir);
+
+    // printf("Digite o numero que desenha inserir\n");
+    // scanf("%d",&inserir);
+    // root = insertNode(root,inserir);
 
     root = insertNode(root,10);
     root = insertNode(root,7);
     root = insertNode(root,12);
     root = insertNode(root,22); 
     root = insertNode(root,3);
-   
-   printTree(root);
+    
+    root = delete(root,22);
+
+    printTree(root);
 
     Nodo* temp = searchNode(root,22);
     verify(temp);
